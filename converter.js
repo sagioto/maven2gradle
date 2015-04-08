bbd876a8bd1fcb1b95ca08743c062a3970eac5d6
Sagi Bernstein <sbernstein@vmware.com>
2015-04-08 18:50:24 +0300

    Initial commit

---
+++ converter.js
@@ -0,0 +1,37 @@
+/**
+ * Created by sbernstein on 4/8/15.
+ */
+function convert(){
+    var text = document.getElementById('input').value;
+    var output = document.getElementById('output');
+    var parser = new DOMParser();
+    var parsedDeps = parser.parseFromString(text, 'text/xml');
+    var shouldAddOuterClosure =  parsedDeps.getElementsByTagName('dependencies').length;
+    if(!shouldAddOuterClosure){
+        text = '<root>' + text + '</root>';
+        parsedDeps = parser.parseFromString(text, 'text/xml');
+    }
+    var depElems = parsedDeps.getElementsByTagName('dependency');
+    var grDeps = [];
+    for(var i = 0; i < depElems.length; i++) {
+        var depElem = depElems[i];
+        var scopeElems = depElem.getElementsByTagName('scope');
+        var scope = 'compile';
+        if (scopeElems.length && scopeElems[0].innerHTML == 'test') {
+            scope = 'testCompile';
+        }
+        var group = depElem.getElementsByTagName('groupId')[0].innerHTML;
+        var artifact = depElem.getElementsByTagName('artifactId')[0].innerHTML;
+        var versionElems = depElem.getElementsByTagName('version');
+        var version = '*';
+        if (versionElems.length) {
+            version = versionElems[0].innerHTML;
+        }
+        grDeps.push(scope + ' ' + '"' + group + ":" + artifact + ":" + version + '"');
+    }
+    var grDepsOutput = grDeps.join('\n');
+    if(shouldAddOuterClosure){
+        grDepsOutput = 'dependencies {\n\t' + grDepsOutput.replace(/\n/g, '\n\t') + '\n}'
+    }
+    output.value = grDepsOutput;
+}
